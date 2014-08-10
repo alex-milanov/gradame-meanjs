a7f2e225531e97c2a026aa09d785df6f973e5d20
Nikolay Nikolov <npn@cloudxcel.com>
2014-08-10 15:13:44 +0300

    reformatted all whitespace using the grunt tasks

--- public/js/controllers/nav.js
+++ public/js/controllers/nav.js
@@ -1,19 +1,19 @@
 'use strict';
- 
+
 app.controller('NavCtrl', function ($scope, $location, Auth) {
-    
+
     $scope.user = {};
-    
+
     $scope.signedIn = function(){
-    	if(Auth.signedIn()){
-    		$scope.user = Auth.me();
-    		return true;
-    	}
-    	return false;
+      if(Auth.signedIn()){
+        $scope.user = Auth.me();
+        return true;
+      }
+      return false;
     }
 
     $scope.logout = Auth.logout;
 
- 
- 
+
+
 });
--- public/js/controllers/signals.js
+++ public/js/controllers/signals.js
@@ -3,157 +3,157 @@
 
 app.controller('SignalsCtrl', function ($scope, $location, $http, $timeout, Signal, Maps) {
 
-	
-	//$scope.signal = new Signal();
-	$scope.signals = [];
+
+  //$scope.signal = new Signal();
+  $scope.signals = [];
 
 
-	$scope.signalTypes = [
-		"Улична дупка",
-		"Липсваща шахта",
-		"Висящи кабели",
-		"Нерегламентиран боклук",
-		"Вандализъм"
-	];
+  $scope.signalTypes = [
+    "Улична дупка",
+    "Липсваща шахта",
+    "Висящи кабели",
+    "Нерегламентиран боклук",
+    "Вандализъм"
+  ];
 
-	$scope.signalStatuses = [
-		"отворен",
-		"решен"
-	];
+  $scope.signalStatuses = [
+    "отворен",
+    "решен"
+  ];
 
-	$scope.filter = {
-		bounds: "",
-		location: "",
-		type: "",
-		status: ""
-	};
+  $scope.filter = {
+    bounds: "",
+    location: "",
+    type: "",
+    status: ""
+  };
 
-	$scope.$watch('filter.type', function() {
-    	$scope.load();
-	});
-	$scope.$watch('filter.status', function() {
-    	$scope.load();
-	});
-	$scope.$watch('filter.bounds', function() {
-    	$scope.load();
-	});
+  $scope.$watch('filter.type', function() {
+      $scope.load();
+  });
+  $scope.$watch('filter.status', function() {
+      $scope.load();
+  });
+  $scope.$watch('filter.bounds', function() {
+      $scope.load();
+  });
 
-	//$scope.location
+  //$scope.location
 
-	
-	Maps.addListener('positionChanged', function(){
-		if(Maps.getPosition())
-			$scope.filter.location = Maps.getPosition().toString();
-		if(Maps.getBounds())
-			$scope.filter.bounds = Maps.getBounds().toString();
-	});
-	
-	Maps.addListener('boundsChanged', function(){
-		if(Maps.getBounds())
-			$scope.filter.bounds = Maps.getBounds().toString();
-	});
 
-	
-	function displaySignals(list){
+  Maps.addListener('positionChanged', function(){
+    if(Maps.getPosition())
+      $scope.filter.location = Maps.getPosition().toString();
+    if(Maps.getBounds())
+      $scope.filter.bounds = Maps.getBounds().toString();
+  });
 
-		
-		Maps.clearMarkers();
-		
-		for(var i in list){
-			if(list[i].location){
-				
-				var markerOptions = {
-				    position: new google.maps.LatLng(list[i].location[0], list[i].location[1]),
-				    icon: new google.maps.MarkerImage("/img/markers/sign.png")
-					//animation: google.maps.Animation.DROP
-				};
-				
-				Maps.addMarker(markerOptions)
-				
-			}
-		}
-		
-		Maps.updateCluster();
-	}
+  Maps.addListener('boundsChanged', function(){
+    if(Maps.getBounds())
+      $scope.filter.bounds = Maps.getBounds().toString();
+  });
 
-	$scope.init = function(){
-		// after initial state load
-		$timeout(function() {
-			Maps.init($scope.map, document.getElementById('autocomplete'));
-		});
-	}
 
-	$scope.map = {
-	    control: {},
-	    options: {
+  function displaySignals(list){
+
+
+    Maps.clearMarkers();
+
+    for(var i in list){
+      if(list[i].location){
+
+        var markerOptions = {
+            position: new google.maps.LatLng(list[i].location[0], list[i].location[1]),
+            icon: new google.maps.MarkerImage("/img/markers/sign.png")
+          //animation: google.maps.Animation.DROP
+        };
+
+        Maps.addMarker(markerOptions)
+
+      }
+    }
+
+    Maps.updateCluster();
+  }
+
+  $scope.init = function(){
+    // after initial state load
+    $timeout(function() {
+      Maps.init($scope.map, document.getElementById('autocomplete'));
+    });
+  }
+
+  $scope.map = {
+      control: {},
+      options: {
             streetViewControl: false,
             panControl: false,
             maxZoom: 20,
             minZoom: 3
         },
-        center: { 
-	        latitude: 42.7,
-	        longitude: 23.3
+        center: {
+          latitude: 42.7,
+          longitude: 23.3
         },
-	    zoom: 10
-	};
+      zoom: 10
+  };
 
 
 
-	// general load
-	$scope.load = function(params){
-		
-		if(!params)
-			var params = {};
+  // general load
+  $scope.load = function(params){
 
-		// don't autoload
-		if(!$scope.filter.bounds || $scope.filter.bounds == ""){
-			return;
-		} else {
-			params["bounds"] = $scope.filter.bounds;
-		}
+    if(!params)
+      var params = {};
 
-		if($scope.filter.type != ""){
-			params["type"] = $scope.filter.type;
-		}
+    // don't autoload
+    if(!$scope.filter.bounds || $scope.filter.bounds == ""){
+      return;
+    } else {
+      params["bounds"] = $scope.filter.bounds;
+    }
 
-		if($scope.filter.status != ""){
-			params["status"] = $scope.filter.status;
-		}
-		
-		Signal.query(params).$promise.then(function(signals){
-			//console.log(signals);
-			for(var i in signals){
-				var signal = signals[i];
-				
-				// modifiers here
+    if($scope.filter.type != ""){
+      params["type"] = $scope.filter.type;
+    }
 
-				signals[i] = signal;
-			}
-			
-			$scope.signals = signals;
-			displaySignals(signals);
-		});
-		
-	}
+    if($scope.filter.status != ""){
+      params["status"] = $scope.filter.status;
+    }
 
-	$scope.findNear = function(_callback){
-		Signal.findNear({location:$scope.filter.location}).$promise.then(function(signals){
-			//console.log({near: data});
-			for(var i in signals){
-				var signal = signals[i];
-				
-				// modifiers here
+    Signal.query(params).$promise.then(function(signals){
+      //console.log(signals);
+      for(var i in signals){
+        var signal = signals[i];
 
-				signals[i] = signal;
-			}
-			
-			$scope.signals = signals;
-			_callback();
-		})
-	}
+        // modifiers here
 
-	//$scope.load();
-	
+        signals[i] = signal;
+      }
+
+      $scope.signals = signals;
+      displaySignals(signals);
+    });
+
+  }
+
+  $scope.findNear = function(_callback){
+    Signal.findNear({location:$scope.filter.location}).$promise.then(function(signals){
+      //console.log({near: data});
+      for(var i in signals){
+        var signal = signals[i];
+
+        // modifiers here
+
+        signals[i] = signal;
+      }
+
+      $scope.signals = signals;
+      _callback();
+    })
+  }
+
+  //$scope.load();
+
 
 });
