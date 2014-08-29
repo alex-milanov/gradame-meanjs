c3d78018a5d74bb2b1dcc59512df50cbd8522c3e
Nikolay Nikolov <npn@cloudxcel.com>
2014-08-29 16:43:17 +0300

    added grunt tasks to compile the sass stylesheet

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,15 +48,33 @@ module.exports = function(grunt) {
       spaces2tabs: 'find . \\\( -name "*.js" -o -name "*.html" -o -name "*.css" \\\) ' +
                    '-not \\\( -type d -o -path "./public/lib/*" -o -path "./node_modules/*" -o -path "./.vagrant/*" -o -path "./.git/*" \\\) ' +
                    '-exec bash -c \'unexpand -t 2 "$0" > /tmp/e && mv /tmp/e "$0"\' {} \\\;'
-    }
+    },
+
+    sass: {
+			dist: {
+				files: {
+					'public/css/style.css' : 'public/css/style.sass'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: '**/*.sass',
+				tasks: ['sass']
+			}
+		}
   });
 
   grunt.loadNpmTasks('grunt-lintspaces');
   grunt.loadNpmTasks('grunt-trimtrailingspaces');
   grunt.loadNpmTasks('grunt-exec');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('check-spaces', ['lintspaces']);
 
   grunt.registerTask('fix-spaces', ['trimtrailingspaces', 'exec:tabs2spaces']);
+
+  grunt.registerTask('default',['watch']);
 
 };
