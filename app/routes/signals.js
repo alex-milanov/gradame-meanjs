0316b7d7edba4fb7c792bdcbc76917c70d9f90a4
Valentin Slavov <vnslavov@gmail.com>
2014-09-05 19:10:57 +0300

    added connect-multiparty module to handle image uploads

--- app/routes/signals.js
+++ app/routes/signals.js
@@ -3,12 +3,14 @@
 module.exports = function(app) {
   var users = require('../../app/controllers/users');
   var signals = require('../../app/controllers/signals');
+  var multipart = require('connect-multiparty');
+  var multipartMiddleware = multipart();
 
   // Article Routes
   app.get('/api/signals', signals.list);
   app.get('/api/signals/near', signals.findNear);
   //app.post('/signals', users.requiresLogin, signals.create);
-  app.post('/api/signals', users.requiresToken, signals.create);
+  app.post('/api/signals', users.requiresToken, multipartMiddleware, signals.create);
   app.get('/api/signals/:signalId', signals.read);
   //app.put('/signals/:signalId', users.requiresLogin, signals.hasAuthorization, signals.update);
   app.put('/api/signals/:signalId', signals.update);
